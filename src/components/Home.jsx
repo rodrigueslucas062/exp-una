5791b9855196960d7acb28b686a4b811f123ffaa
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-18 21:39:31 -0300

    🚧 Initial texts

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export function Main() {
+    return (
+        <section id="home" className="flex flex-col text-center">
+            <span className="text-3xl mb-4">Expo una</span>
+            <span>em desenvolvimento</span>
+        </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
+import { Main } from "@/components/Home";
+
 export default function Home() {
   return (
-    <main>
-      Expo Una
-    </main>
+    <Main />
   );
 }
