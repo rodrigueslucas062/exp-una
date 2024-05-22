87398777548340e50e9598f28cc09460cb1703c4
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-05-22 19:34:43 -0300

    🚧main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,49 @@
+import { Ellipsis } from "lucide-react";
+import Link from "next/link";
+
 export function Main() {
-    return (
-        <section id="home" className="flex flex-col text-center">
-            <span className="text-3xl mb-4">Expo una</span>
-            <span>em desenvolvimento</span>
-        </section>
-    )
+  return (
+    <section id="home" className="flex flex-col space-y-4 place-items-center">
+      <span className="text-3xl mb-4 ">Expo una</span>
+      <div className="space-y-4 flex flex-col">
+      <Link href={""} className="flex justify-between p-3 rounded-lg w-full bg-white text-zinc-900 font-semibold">
+        <div>
+            LOGO
+        </div>
+        <span>
+            Apresentações
+        </span>
+        <div>
+            <Ellipsis/>
+        </div>
+      </Link>
+
+      <Link href={""} className="flex justify-between p-3 rounded-lg w-full bg-white text-zinc-900 font-semibold">
+        <div>
+            LOGO
+        </div>
+        <span>
+            Workshops
+        </span>
+        <div>
+            <Ellipsis/>
+        </div>
+      </Link>
+
+      <Link href={""} className="flex justify-between p-3 rounded-lg w-full bg-white text-zinc-900 font-semibold">
+        <div>
+            LOGO
+        </div>
+        <span>
+            Plestras
+        </span>
+        <div>
+            <Ellipsis/>
+        </div>
+      </Link>
+      </div>
+      
+      
+    </section>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import { Main } from "@/components/Main";
 
 export default function Home() {
   return (
-    <Main />
+    <main className="flex min-h-screen flex-col bg-[#3B82F6]">
+      <Main />
+    </main>
   );
 }
