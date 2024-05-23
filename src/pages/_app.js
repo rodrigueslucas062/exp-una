770f2f5d059f107dc27205e44d528a9a77d7b260
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-23 15:26:06 -0300

    📈 Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Analytics />
       <SpeedInsights />
       <Navbar />
       <Component {...pageProps} />
