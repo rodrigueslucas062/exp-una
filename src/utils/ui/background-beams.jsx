d5cc331ad494082eadcc7a585fc0d1ea3e71f68f
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-23 19:15:57 -0300

    💫Animation adjustments

--- src/utils/ui/background-beams.jsx
+++ src/utils/ui/background-beams.jsx
@@ -61,12 +61,12 @@ export const BackgroundBeams = React.memo(({ className }) => {
   return (
     <div
       className={cn(
-        "absolute h-full w-full inset-0  [mask-size:40px] [mask-repeat:no-repeat] flex items-center justify-center",
+        "-z-10 absolute h-full w-full inset-0  [mask-size:40px] [mask-repeat:no-repeat] flex items-center justify-center",
         className
       )}
     >
       <svg
-        className=" -z-10 h-full w-full pointer-events-none absolute "
+        className="-z-10 h-full w-full pointer-events-none absolute "
         width="100%"
         height="100%"
         viewBox="0 0 696 316"
