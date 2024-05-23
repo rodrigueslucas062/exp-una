772e9fe417e837739796997fe884f00c47aaaed0
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-22 23:59:41 -0300

    🤡 Deploy test

--- src/utils/ui/background-beams.jsx
+++ src/utils/ui/background-beams.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/utils/ui/cn";
 
-export const BackgroundBeams = React.memo(({ className }) => {
+const BackgroundBeams = React.memo(({ className }) => {
   const paths = [
     "M-380 -189C-380 -189 -312 216 152 343C616 470 684 875 684 875",
     "M-373 -197C-373 -197 -305 208 159 335C623 462 691 867 691 867",
@@ -137,3 +137,5 @@ export const BackgroundBeams = React.memo(({ className }) => {
     </div>
   );
 });
+
+export default BackgroundBeams
