819c58c511c11161689b8de67f0624c89b73ccb7
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-05-23 11:20:34 -0300

    🩹 SpeedInsights import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,11 @@
 import Navbar from "@/components/navbar/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <SpeedInsights />
+      <SpeedInsights />
       <Navbar />
       <Component {...pageProps} />
     </>
