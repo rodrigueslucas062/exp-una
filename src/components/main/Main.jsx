d1a156e43a0d2bd13e3119ae39fd7dcb771fd967
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-23 23:02:30 -0300

    💫 Transition of links when starting

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,12 @@
+import { motion } from "framer-motion";
 import Links from "./links";
 
 export function Main() {
   return (
-    <section id="expoUna" className="rounded-2xl pt-24">
+    <motion.section id="expoUna" className="rounded-2xl pt-24"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.9 }} >
       <div className="flex flex-col items-center pt-24">
         <span className="mb-1 text-xl font-medium text-white">Expo Una</span>
         <span className="text-sm font-semibold mb-6 text-gray-300">texto de teste</span>
@@ -10,6 +14,6 @@ export function Main() {
       <div className="flex flex-col items-center space-y-4">
         <Links />
       </div>
-    </section>
+    </motion.section>
   );
 }
