0aa7675fcbc0d917e61e1eed3d0e17c2b6f3bc49
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-27 22:23:48 -0300

    ⚰️ Removal of unused code

--- src/hooks/use-in-view.hook.js
+++
@@ -1,9 +0,0 @@
-import { useInView } from 'react-intersection-observer';
-
-export const useInViewHook = (viewPercentage) => {
-    const [ref, inView] = useInView({
-        triggerOnce: true, // Trigger the animation only once
-        threshold: viewPercentage, // Trigger when 50% of the element is in view
-    });
-    return { ref, inView };
-};
