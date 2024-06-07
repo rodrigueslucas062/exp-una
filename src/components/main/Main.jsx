2e674f0f901d439fd9c2d14b098d78dba64291f9
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-06-07 00:25:19 -0300

    🤡Home text test

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Links from "./links";
+import Link from "next/link";
 
 export function Main() {
   return (
@@ -8,8 +9,10 @@ export function Main() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.9 }} >
       <div className="flex flex-col items-center pt-24">
-        <span className="mb-1 text-xl font-medium text-white">Expo Una</span>
-        <span className="text-sm font-semibold mb-6 text-gray-300">texto de teste</span>
+        <span className="mb-6 text-3xl font-medium text-white">Expo Una</span>
+        <span className="text-sm font-semibold mb-4 text-gray-300">Programação da ExpoUna das turmas de TI</span>
+        <span className="text-sm font-semibold mb-4 text-gray-300">Não deixe de conferir nosso <Link href={''} className="text-purple-500 font-bold">Sympla</Link></span>
+        <span className="text-sm font-semibold mb-4 text-gray-300">Participe para ganhar <span className="text-purple-500 font-bold">10h de extensão</span></span> 
       </div>
       <div className="flex flex-col items-center space-y-4">
         <Links />
