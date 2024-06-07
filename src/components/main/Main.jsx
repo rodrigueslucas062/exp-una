4a07597afcc8793bfba072b0deca70e3c5acc087
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-06-07 13:58:28 -0300

    💬update text and literals

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -10,12 +10,14 @@ export function Main() {
       transition={{ duration: 0.9 }} >
       <div className="flex flex-col items-center pt-24">
         <span className="mb-6 text-3xl font-medium text-white">Expo Una</span>
-        <span className="text-sm font-semibold mb-4 text-gray-300">Programação da ExpoUna das turmas de TI</span>
-        <span className="text-sm font-semibold mb-4 text-gray-300">Não deixe de conferir nosso <Link href={''} className="text-purple-500 font-bold">Sympla</Link></span>
         <span className="text-sm font-semibold mb-4 text-gray-300">Participe para ganhar <span className="text-purple-500 font-bold">10h de extensão</span></span> 
       </div>
       <div className="flex flex-col items-center space-y-4">
         <Links />
+      </div>
+      <div className="flex flex-col items-center pt-6">
+      <span className="text-sm font-semibold mb-4 text-gray-300">Programação da ExpoUna das turmas de TI</span>
+      <span className="text-sm font-semibold mb-4 text-gray-300">Não deixe de conferir nosso <Link href={''} className="text-purple-500 font-bold">Sympla</Link></span>
       </div>
     </motion.section>
   );
