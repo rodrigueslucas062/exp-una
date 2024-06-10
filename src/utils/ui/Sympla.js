ed8d81cdec985150620149e862a6ff945e6970bf
Lucas Rodrigues <39422637+rodrigueslucas062@users.noreply.github.com>
2024-06-10 11:41:40 -0300

    ✨Update Sympla.js

--- src/utils/ui/Sympla.js
+++ src/utils/ui/Sympla.js
@@ -5,7 +5,7 @@ import Sympla from "../../../public/images/SymplaIcon.jpeg";
 export default function SymplaButton() {
   return (
     <>
-      <Link href={""} className="flex items-center gap-2 py-2 px-3 backdrop-blur-sm bg-zinc-100/[0.03] rounded-md" >
+      <Link href={"https://www.sympla.com.br/evento/expouna/2500389"} className="flex items-center gap-2 py-2 px-3 backdrop-blur-sm bg-zinc-100/[0.03] rounded-md" >
         <Image src={Sympla} className="rounded-full size-6" />
         <span className="text-sm font-semibold text-white">Sympla</span>
       </Link>
