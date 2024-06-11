aec8cd209992d42eade0f567574177ea9de080f3
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-06-11 00:17:37 -0300

    🎨 Improvements in text layout and rendering

--- src/components/workshop/workshoplist.js
+++ src/components/workshop/workshoplist.js
@@ -15,31 +15,6 @@ const lista = [
     sala: "1105",
     horario: "08:50-09:50",
     dia: "Segunda-feira, 17/06",
-    detalhes: `HTML é a linguagem de 
-      <span className="text-purple-500 font-bold">
-        marcação utilizada para estruturar o conteúdo na web
-      </span>
-      . Ele define a 
-      <span className="text-purple-500 font-bold">
-        semântica dos elementos
-      </span> 
-      que compõem uma página, como parágrafos, cabeçalhos, e muito mais. 
-      <span className="text-purple-500 font-bold">
-        HTML usa tags para encapsular diferentes partes do conteúdo
-      </span>
-      , permitindo que os navegadores exibam o conteúdo de forma organizada. CSS 
-      <span className="text-purple-500 font-bold">
-        é a linguagem de estilo utilizada para definir a aparência e o layout
-      </span> 
-      das páginas web. Com CSS, você pode controlar aspectos visuais como cores, fontes, espaçamento, alinhamento e disposição dos elementos na tela.
-      CSS permite a separação entre o conteúdo (HTML) e a apresentação visual, facilitando a manutenção e a atualização do design de um site. 
-      <span className="text-purple-500 font-bold">
-        CSS é aplicado aos elementos HTML através de seletores
-      </span> 
-      que podem ser baseados em 
-      <span className="text-purple-500 font-bold">
-        tags, classes, IDs ou atributos
-      </span>.`,
   },
   {
     img: Images.Git,
@@ -49,7 +24,6 @@ const lista = [
     sala: "1106",
     horario: "10:30-11:30",
     dia: "Segunda-feira, 17/06",
-    detalhes: "Ainda em elaboração",
   },
   {
     img: Images.Sql,
@@ -59,7 +33,6 @@ const lista = [
     sala: "1105",
     horario: "10:30-11:30",
     dia: "terça-feira, 18/06",
-    detalhes: "Ainda em elaboração",
   },
   {
     img: Images.Excel,
@@ -69,7 +42,6 @@ const lista = [
     sala: "1105",
     horario: "10:30-11:30",
     dia: "Sexta-feira 21/06",
-    detalhes: "Ainda em elaboração",
   },
   {
     img: Images.Blender,
@@ -79,9 +51,92 @@ const lista = [
     sala: "206",
     horario: "10:30-11:30",
     dia: "Quarta-feira 19/06",
-    detalhes: "Ainda em elaboração",
   },
 ];
+
+function DetalhesWorkshop({ titulo }) {
+  switch (titulo) {
+    case "HTML e CSS":
+      return (
+        <div className="pb-6">
+          <p>
+            HTML é a linguagem de
+            <span className="text-purple-500 font-bold">
+              marcação utilizada para estruturar o conteúdo na web
+            </span>
+            . Ele define a
+            <span className="text-purple-500 font-bold">
+              semântica dos elementos
+            </span>
+            que compõem uma página, como parágrafos, cabeçalhos, e muito mais.
+            <span className="text-purple-500 font-bold">
+              HTML usa tags para encapsular diferentes partes do conteúdo
+            </span>
+            , permitindo que os navegadores exibam o conteúdo de forma
+            organizada. CSS
+            <span className="text-purple-500 font-bold">
+              é a linguagem de estilo utilizada para definir a aparência e o
+              layout
+            </span>
+            das páginas web. Com CSS, você pode controlar aspectos visuais como
+            cores, fontes, espaçamento, alinhamento e disposição dos elementos
+            na tela. CSS permite a separação entre o conteúdo (HTML) e a
+            apresentação visual, facilitando a manutenção e a atualização do
+            design de um site.
+            <span className="text-purple-500 font-bold">
+              CSS é aplicado aos elementos HTML através de seletores
+            </span>
+            que podem ser baseados em
+            <span className="text-purple-500 font-bold">
+              tags, classes, IDs ou atributos
+            </span>
+            .
+          </p>
+        </div>
+      );
+    case "Git":
+      return (
+        <div>
+          <p>
+            Torne-se um especialista em Git, a ferramenta essencial para
+            controle de versão de software. Aprenda a trabalhar em equipe com
+            eficiência, rastreando alterações e versionamento do seu código.
+          </p>
+        </div>
+      );
+    case "SQL":
+      return (
+        <div>
+          <p>
+            Domine a linguagem SQL para gerenciar e consultar bancos de dados.
+            Aprenda a armazenar, organizar e recuperar informações com maestria.
+          </p>
+        </div>
+      );
+    case "Excel":
+      return (
+        <div>
+          <p>
+            Transforme-se em um mestre de planilhas com o Excel. Aprenda a
+            organizar, analisar e visualizar dados de forma eficiente,
+            otimizando seu tempo e trabalho.
+          </p>
+        </div>
+      );
+    case "Blender":
+      return (
+        <div>
+          <p>
+            Dê vida à sua imaginação com o Blender, um software de modelagem 3D
+            gratuito e poderoso. Aprenda os fundamentos da criação de modelos
+            3D, animações e efeitos visuais.
+          </p>
+        </div>
+      );
+    default:
+      return null;
+  }
+}
 
 export default function Apresentacao() {
   return (
@@ -95,7 +150,6 @@ export default function Apresentacao() {
             sala={info.sala}
             dia={info.dia}
             horario={info.horario}
-            detalhes={info.detalhes}
           />
         </div>
       ))}
@@ -103,28 +157,11 @@ export default function Apresentacao() {
   );
 }
 
-function MaterialComplementar({ texto }) {
-  const handleClick = () => {
-    toast.success('Disponível na semana do evento', {
-      position: 'bottom-center',
-      duration: 2000,
+const handleClick = () => {
+  toast.success("Disponível na semana do evento", {
+    position: "bottom-center",
+    duration: 2000,
   })
-  };
-
-  return (
-    <div className="flex flex-col items-center space-y-4">
-      <button 
-        className="flex w-[95%] justify-between px-3 rounded-md ring-1 ring-gray-300 py-2 lg:py-3"
-        onClick={handleClick}
-      >
-        <Library />
-        <span>Material complementar</span>
-        <ChevronRight />
-      </button>
-      <p className="text-wrap text-center pb-4" dangerouslySetInnerHTML={{ __html: texto }}>
-      </p>
-    </div>
-  );
 }
 
 function ApresentacaoWorkshop({ imagem, titulo, descricao, sala, dia, horario, detalhes }) {
@@ -132,7 +169,10 @@ function ApresentacaoWorkshop({ imagem, titulo, descricao, sala, dia, horario, d
 
   return (
     <Dialog.Root>
-      <div className="flex flex-col lg:flex-row rounded-xl bg-zinc-800 overflow-hidden" style={shadowStyle} >
+      <div
+        className="flex flex-col lg:flex-row rounded-xl bg-zinc-800 overflow-hidden"
+        style={shadowStyle}
+      >
         <div className="hidden lg:block lg:w-1/3 h-64 relative bg-purple-500/[0.3] rounded-full">
           <Image
             alt="Album cover"
@@ -168,7 +208,7 @@ function ApresentacaoWorkshop({ imagem, titulo, descricao, sala, dia, horario, d
           <div className="flex mt-4 md:mt-auto flex-col">
             <div className="flex justify-between pt-4">
               <Dialog.Trigger className="text-small text-white">
-                <span>Saiba mais</span>
+                <span>Mais Informações</span>
               </Dialog.Trigger>
               <div className="flex gap-2 items-center text-small text-white">
                 <SymplaButton />
@@ -194,7 +234,12 @@ function ApresentacaoWorkshop({ imagem, titulo, descricao, sala, dia, horario, d
                             </span>
                           </div>
                           <div className="flex flex-col items-center space-y-4">
-                            <MaterialComplementar texto={detalhes} />
+                            <button className="flex w-[95%] justify-between px-3 rounded-md ring-1 ring-gray-300 py-2 lg:py-3" onClick={handleClick} >
+                              <Library />
+                              <span>Material complementar</span>
+                              <ChevronRight />
+                            </button>
+                            <DetalhesWorkshop titulo={titulo} />
                           </div>
                         </div>
                       </div>
